28e0049838ca5cfe90648c96eb7c6126704d9d71
Joshua Frasier <jafrasier50@hotmail.com>
2018-05-01 14:03:10 -0500

    finally working i hope

---
+++ name.py
@@ -0,0 +1,5 @@
+print("Suh, dude?")
+first_name = str(input("Please state your first name: "))
+last_name = str(input("Now, please state your last name: "))
+
+print(f"Hello! My name is, {first_name} {last_name}. I love eggs. You should too.")
